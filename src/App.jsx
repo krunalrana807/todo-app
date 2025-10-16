65cb23f977f6e6c03796759cc6a61df32dc19de2
Krunal Rana <krunalrana807@gmail.com>
2025-10-16 17:19:01 +0530

    remove container

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Header from "./component/Header";
-import Todoitem from "./component/Todoitem";
-import Counter from "./component/Counter";
-import Useeffect from "./component/Useeffect";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './component/Header'
+import Todoitem from './component/Todoitem'
+import Counter from './component/Counter'
+import Useeffect from './component/Useeffect'
 
 function App() {
+
+
   return (
     <>
-      <div className="container">
-        <div className="todo-container">
-          <div className="page-counter">
-            <Counter />
-          </div>
-          <div className="main">
-            <div className="header">
-              <Header />
-            </div>
-            <div className="item">
-              <Todoitem />
-            </div>
-          </div>
+    <div className='container'>
+      
+    </div>
+      <div className='todo-container'>
+        <div className='page-counter'>
+           <Counter />
         </div>
-        {/* <Useeffect /> */}
+        <div className='main'>
+          <div className='header'>
+            <Header /> 
+          </div>
+      <div className='item'>
+        <Todoitem />
       </div>
+        </div>
+      </div>
+      {/* <Useeffect /> */}
     </>
-  );
+  )
 }
 
-export default App;
+export default App
